747c1f0b33789aa6fe17d3d46e0d1757cab6ffee
Tuncay Basak <tuncay.basak@outlook.com>
2024-02-03 03:17:01 +0100

    bs syntax tree

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,40 @@
+declare const syntax: {
+    types: {
+        empty: string;
+        number: string;
+        string: string;
+        boolean: string;
+        array: string;
+    };
+    keywords: {
+        method: string;
+        struct: string;
+        impl: string;
+        pub: string;
+        get: string;
+        as: string;
+        fn: string;
+        async: string;
+        await: string;
+        if: string;
+        else: string;
+        match: string;
+        for: string;
+        in: string;
+        of: string;
+        while: string;
+        do: string;
+        let: string;
+        const: string;
+        mut: string;
+    };
+    identifiers: {
+        type: string;
+        fn: string;
+        method: string;
+        struct: string;
+        var: string;
+    };
+};
+export default syntax;
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,41 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const syntax = {
+    types: {
+        empty: "()",
+        number: "number",
+        string: "string",
+        boolean: "bool",
+        array: "[]"
+    },
+    keywords: {
+        method: "method",
+        struct: "struct",
+        impl: "impl",
+        pub: "pub",
+        get: "get",
+        as: "as",
+        fn: "fn",
+        async: "async",
+        await: "await",
+        if: "if",
+        else: "else",
+        match: "match",
+        for: "for",
+        in: "in",
+        of: "of",
+        while: "while",
+        do: "do",
+        let: "let",
+        const: "const",
+        mut: "mut",
+    },
+    identifiers: {
+        type: "type",
+        fn: "fn",
+        method: "method",
+        struct: "struct",
+        var: "var",
+    }
+};
+exports.default = syntax;
